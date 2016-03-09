1b35c3e90d5aae7b9b7b4e0f56c0f9467d1b4430
Marko Valečić <marko.valecic@skole.hr>
2016-03-09 13:36:48 +0100

    Dodali smo jos jednu varijablu.

--- index.php
+++ index.php
@@ -2,11 +2,36 @@
 <html
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8">
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
 
+<?php
+
+    echo('Ovo je prije odlomka<br>');
+    echo('Ovo je jos dodano');
+
+    echo(date ('d.m.Y'));
+    echo('<br>');
+
+    $ocjena = 5;
+    echo('Vaša je ocjena :' . $ocjena);
+    echo('<br>');
+
+?>
+
+
+<?php
+
+    echo('Ovo je drugi odlomak<br>');
+
+
+
+
+
+?>
 </p>
 
     Ovo je odlomak
