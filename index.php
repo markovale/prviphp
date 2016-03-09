866ae20c28c76e2de51e4df5cd819ea31d46ba64
Marko Valečić <marko.valecic@skole.hr>
2016-03-09 13:54:15 +0100

    Dodali smo formulu za zbrajanje.

--- index.php
+++ index.php
@@ -8,6 +8,17 @@
 <body>
 <h1>Prvi PHP</h1>
 
+<form>
+    Prvi broj: <br>
+    <input type="text" name="broj1"><br>
+    Drugi broj: <br>
+    <input type="text" name="broj2"><br>
+    <br>
+    <input type="submit" value="Izračunaj">
+
+</form>
+
+
 <?php
 
     echo('Ovo je prije odlomka<br>');
@@ -16,23 +27,23 @@
     echo(date ('d.m.Y'));
     echo('<br>');
 
-    $ocjena = 5;
-    echo('Vaša je ocjena :' . $ocjena);
+    $rezultat = $_GET['broj1'] + $_GET['broj2'];
+    echo('<br>' . $rezultat);
     echo('<br>');
 
+
+
+   // $ocjena = 5;
+    //echo('Vaša je ocjena :' . $ocjena);
+    //$nova_var = 166.8;
+    //echo('<br>' . $nova_var);
+
+    //$rezultat = $ocjena + $nova_var;
+    //echo('<br>' . $rezultat);
+
 ?>
 
 
-<?php
-
-    echo('Ovo je drugi odlomak<br>');
-
-
-
-
-
-?>
-</p>
 
     Ovo je odlomak
 
