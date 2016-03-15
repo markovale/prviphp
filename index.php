389f3e231d5487fc0ea8203914ca1c0480987205
Marko Valečić <marko.valecic@skole.hr>
2016-03-15 18:00:56 +0100

    Podjelili smo formu na dvije stranice

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!doctype html>
-<html
+<!DOCTYPE html>
+<html>
 <head>
     <title>Prvi PHP</title>
     <meta charset="utf-8">
@@ -8,44 +8,26 @@
 <body>
 <h1>Prvi PHP</h1>
 
-<form>
-    Prvi broj: <br>
+<form method ="post" action="rezultat.php">
+    Prvi broj:<br>
     <input type="text" name="broj1"><br>
-    Drugi broj: <br>
+    Drugi broj:<br>
     <input type="text" name="broj2"><br>
+    Treci broj:<br>
+    <input type="text" name="broj3"><br>
+    <br>
+    <input type="text" name="ime"><br>
     <br>
     <input type="submit" value="Izračunaj">
-
 </form>
 
 
-<?php
-
-    echo('Ovo je prije odlomka<br>');
-    echo('Ovo je jos dodano');
-
-    echo(date ('d.m.Y'));
-    echo('<br>');
-
-    $rezultat = $_GET['broj1'] + $_GET['broj2'];
-    echo('<br>' . $rezultat);
-    echo('<br>');
+<p>
+</p>
+</body>
+</html>
 
 
-
-   // $ocjena = 5;
-    //echo('Vaša je ocjena :' . $ocjena);
-    //$nova_var = 166.8;
-    //echo('<br>' . $nova_var);
-
-    //$rezultat = $ocjena + $nova_var;
-    //echo('<br>' . $rezultat);
-
-?>
-
-
-
-    Ovo je odlomak
 
 </p>
 </body>
